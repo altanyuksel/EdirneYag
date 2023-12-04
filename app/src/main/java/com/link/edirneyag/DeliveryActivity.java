81b6948a4d477143fd7654dc71c1e1c8a70e8df8
altanyuksel <altan_518@hotmail.com>
2023-12-04 17:47:08 +0300

    çalışan versiyon.

--- app/src/main/java/com/link/edirneyag/DeliveryActivity.java
+++ app/src/main/java/com/link/edirneyag/DeliveryActivity.java
@@ -1,13 +1,59 @@
 package com.link.edirneyag;
 
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.view.SurfaceHolder;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class DeliveryActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.exit))
+                .setCancelable(false)
+                .setPositiveButton(getApplicationContext().getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        finishAffinity();
+                        System.exit(0);
+                    }
+                })
+                .setNegativeButton(getApplicationContext().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
 
--- app/src/main/java/com/link/edirneyag/LoginActivity.java
+++ app/src/main/java/com/link/edirneyag/LoginActivity.java
@@ -109,7 +109,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         initViews();
         initDefinitions();
         try {
